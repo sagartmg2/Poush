5d3734de87b0abbad6777685bbd77ec4cef886eb
Sagar <dev.sagartmg@gmail.com>
2023-01-26 17:11:09 +0545

    javascript

---
+++ js/index.js
@@ -0,0 +1,28 @@
+/* DATA types */
+
+/* 
+    OOP - class 
+    js - not strict OOP - can also work with functions
+*/
+
+
+// DATA TYPES
+
+// text - String
+// number - Int, Float , Double, Decimal - Number
+// true/false - Boolean
+
+
+
+
+
+console.log("Hello World !")
+
+course = 'MERN'
+age = 10
+status = false
+
+console.log("course = " + course)
+console.log(age)
+
+
