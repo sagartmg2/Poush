428c703a795e05ca2440187285221773423930e3
Sagar <dev.sagartmg@gmail.com>
2023-01-27 17:09:49 +0545

    variable and data-types

---
+++ js/variable.js
@@ -0,0 +1,173 @@
+console.log("Hello Wordl")
+
+// int age = 12;
+// age = 12
+// $age = 12
+
+/* Datype
+    String - text
+    Number - int, float,  
+    Boolean   - true/ false
+    undefined
+    null
+*/
+
+color = "blue";
+console.log("color", color);
+
+let age = 12
+console.log(age);
+let name = "Ram"
+console.log(name);
+
+var company = "Mindrisers"
+console.log(company);
+
+
+/* 
+
+*/
+
+var course; // declartion 
+course = "MERN" // initialization
+console.log(course);
+
+/* Ecmascript */
+/* 
+    es6  (2015)
+        - let 
+        - const 
+*/
+
+
+var watch; // declartion 
+var watch = "CASIO" // declartion and initialization
+console.log(watch);
+
+let version;
+// let version = 10; // cannot re-declare
+version = 10;
+version = 11;
+version = 12;
+version = 14;
+console.log("version - ", version);
+
+/* 
+    let vs var
+
+    var - unlimited number of times declare
+    let - only once declartion
+*/
+
+
+// const PI = 3.14 // declartion & initaliztion  
+// console.log(PI);
+
+const PI = 3.14;
+// PI = 1231; // cannot re-assign constant variables.
+
+
+let products_data = null
+console.log("products_data", products_data);
+
+
+let orders_data;
+console.log("orders-data - ", orders_data);
+
+
+/* DATA type
+        Primitive Data type
+            - String
+            number
+            Boolean
+            null
+            undefined 
+        
+        Non-Primiteve data types
+            Array
+            object
+
+
+*/
+
+/* Naming */
+
+let a = 1212; // XX
+let number = 1212;
+
+let price = 100.120
+console.log(price);
+
+// let dashboard_count =  5 // snake_case
+let dashboardCount = 5 // camelCase
+// let border-color = "black" // kabab case
+let orderColor = "black" // camelCase
+
+
+let marriage_status = true;
+console.log("married - ", marriage_status);
+
+
+/* 
+    Non primitive datatype // collection // reference data types -  - collection of  data types
+        - Array  - collection of similar datatypes
+        - Object  - collection of different dattypes
+*/
+
+let fruit1 = "apple"
+let fruit2 = "orange"
+
+
+/* task - to store multiple values in a single variable  */
+
+// let fruits = "apple,orange" // XX
+// let fruits = "apple","orange"
+// let ages = 12,10
+
+let fruits = ["apple", "orange"] // Array
+console.log("furits", fruits)
+
+let ages = [10, 12]
+console.log("ages", ages);
+
+/* 
+    object - real life object 
+
+    syntax
+    let <varaible_name> = { 
+        <key>: <value>
+     }
+
+*/
+
+let projector_1 = {
+    "color": "black",
+    brands: ["samsung", "lg",1111]
+}
+
+console.log("projector", projector_1);
+
+let projector_2 = {
+    "color": "WHITE",
+    brands: ["samsung", "lg"]
+}
+
+let projectors = [projector_1, projector_2]
+console.log("projector", projectors);
+
+
+/* 
+    make a course object  - duration, timing, price, active
+    make a courses array
+*/
+
+
+
+
+
+
+
+
+
+
+
