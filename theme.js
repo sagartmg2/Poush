d30da21f0e1a92d6b2888e342605bd6d8afa0f95
Sagar <dev.sagartmg@gmail.com>
2023-02-06 10:57:53 +0545

    theme and todo

--- theme.js
+++ theme.js
@@ -1,9 +1,33 @@
 console.log("them.js");
 
 function changeToDarkTheme() {
-    console.log("to dark theme.");
-    // dom manipulation
+    document.getElementsByTagName("body")[0].style.backgroundColor = "black"
+    document.getElementsByTagName("body")[0].style.color = "white"
+}
+function changeToLightTheme() {
+    document.getElementsByTagName("body")[0].style.backgroundColor = "white"
+    document.getElementsByTagName("body")[0].style.color = "black"
+}
 
+function changeTheme(type) {
+    let color;
+    let bg_color;
+    let dark = false;
+
+    if (type == "dark") {
+        color = "white";
+        bg_color = "black"
+        document.getElementsByTagName("body")[0].classList.add("dark")
+        // document.getElementsByTagName("body")[0].className = "dark"
+    } else {
+        color = "black";
+        bg_color = "white"
+        document.getElementsByTagName("body")[0].classList.remove("dark")
+
+    }
+
+    // document.getElementsByTagName("body")[0].style.backgroundColor = bg_color
+    // document.getElementsByTagName("body")[0].style.color = color
 }
 
 
